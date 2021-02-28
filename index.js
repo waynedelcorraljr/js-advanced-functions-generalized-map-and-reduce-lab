2f66533c894ae1fd04fb149531d1749f70c045f4
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-28 10:43:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(array, block) {
+    let newArr = [] 
+    for(const element of array) {
+        newArr.push(block(element));
+    }
+    return newArr;
+}
+
+function reduce(array, func, startingPoint = 0) {
+    let newVal = 0;
+    for(const element of array) {
+        if(!!element && typeof element === 'number') {
+            newVal += element;
+        } else if(!!element) {
+            newVal = true;
+        } else {
+            newVal = false;
+        }
+    }
+    if(typeof newVal === 'number') {
+        return newVal + startingPoint;
+    } else {
+        return newVal; 
+    }
+}
